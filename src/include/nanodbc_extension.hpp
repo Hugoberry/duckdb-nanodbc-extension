0c60f60eab0ce1344a343476d03d95c4b381f3e9
Igor Cotruta <hugoberry314@gmail.com>
2025-09-03 18:41:09 +0100

    support new+old API

--- src/include/nanodbc_extension.hpp
+++ src/include/nanodbc_extension.hpp
@@ -6,6 +6,9 @@ namespace duckdb {
 
 class ExtensionLoader; // Forward declaration
 
+// Check if we're using the new extension API
+#ifdef DUCKDB_CPP_EXTENSION_ENTRY
+// New API - standalone class with static methods
 class NanodbcExtension {
 public:
     static void Load(ExtensionLoader &loader);
@@ -22,5 +25,25 @@ public:
 #endif
     }
 };
+
+#else
+// Old API - inherit from Extension base class
+class NanodbcExtension : public Extension {
+public:
+    void Load(DuckDB &db) override;
+    
+    std::string Name() override {
+        return "nanodbc";
+    }
+    
+    std::string Version() const override {
+#ifdef EXT_VERSION_NANODBC
+        return EXT_VERSION_NANODBC;
+#else
+        return "";
+#endif
+    }
+};
+#endif
 
 } // namespace duckdb
--- src/nanodbc_extension.cpp
+++ src/nanodbc_extension.cpp
@@ -4,11 +4,15 @@
 #include "odbc_scanner.hpp"
 #include "duckdb/catalog/catalog.hpp"
 #include "duckdb/parser/parsed_data/create_table_function_info.hpp"
+#ifndef DUCKDB_CPP_EXTENSION_ENTRY
+#include "duckdb/main/extension_util.hpp"
+#endif
 
 namespace duckdb {
 
+// New API version - uses ExtensionLoader
+#ifdef DUCKDB_CPP_EXTENSION_ENTRY
 static void RegisterOdbcFunctions(ExtensionLoader &loader) {
-    // Register each function directly via ExtensionLoader
     loader.RegisterFunction(OdbcScanFunction());
     loader.RegisterFunction(OdbcAttachFunction());
     loader.RegisterFunction(OdbcQueryFunction());
@@ -24,14 +28,41 @@ void NanodbcExtension::Load(ExtensionLoader &loader) {
     LoadInternal(loader);
 }
 
+#else
+// Old API version - uses DatabaseInstance and ExtensionUtil
+static void RegisterOdbcFunctions(DatabaseInstance &instance) {
+    ExtensionUtil::RegisterFunction(instance, OdbcScanFunction());
+    ExtensionUtil::RegisterFunction(instance, OdbcAttachFunction());
+    ExtensionUtil::RegisterFunction(instance, OdbcQueryFunction());
+    ExtensionUtil::RegisterFunction(instance, OdbcExecFunction());
+}
+
+static void LoadInternal(DatabaseInstance &instance) {
+    // Register the ODBC functions
+    RegisterOdbcFunctions(instance);
+}
+
+void NanodbcExtension::Load(DuckDB &db) {
+    LoadInternal(*db.instance);
+}
+#endif
+
 } // namespace duckdb
 
 extern "C" {
 
-// Use the new macro for defining the entry point
+#ifdef DUCKDB_CPP_EXTENSION_ENTRY
+// New API entry point
 DUCKDB_CPP_EXTENSION_ENTRY(nanodbc, loader) {
     duckdb::NanodbcExtension::Load(loader);
 }
+#else
+// Old API entry point
+DUCKDB_EXTENSION_API void nanodbc_init(duckdb::DatabaseInstance &db) {
+    duckdb::DuckDB db_wrapper(db);
+    db_wrapper.LoadExtension<duckdb::NanodbcExtension>();
+}
+#endif
 
 DUCKDB_EXTENSION_API const char *nanodbc_version() {
     return duckdb::DuckDB::LibraryVersion();
